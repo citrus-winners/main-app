de418091b31b0bfed8797b27154ded15879b6d2e
Eddie Zhang <eddie@allthenticate.net>
2021-04-11 03:08:43 -0700

    add at protocol

--- atprotocol/lib/main.dart
+++ atprotocol/lib/main.dart
@@ -1,114 +1,172 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:core';
+import 'package:at_client_mobile/at_client_mobile.dart';
+import 'package:at_server_status/at_server_status.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:at_commons/at_commons.dart';
+import 'package:at_demo_data/at_demo_data.dart' as at_demo_data;
+import 'package:at_client/src/util/encryption_util.dart'; // TODO export this file for its usage PR created
 
+class AtConfig {
+  static const String root = 'vip.ve.atsign.zone';
+  static const int rootPort = 64;
+  static const String namespace = 'server_demo';
+  static const bool debug = true;
+  static const bool verbose = true;
+}
+
+class AtService {
+  static final AtService _singleton = AtService._internal();
+
+  AtService._internal();
+
+  factory AtService.getInstance() => _singleton;
+
+  AtClientService atClientServiceInstance;
+  AtClientImpl atClientInstance;
+  Map<String, AtClientService> atClientServiceMap = {};
+  String _atsign;
+
+  _sync() async => await _getAtClientForAtsign().getSyncManager().sync();
+
+  AtClientImpl _getAtClientForAtsign({String atsign}) {
+    atsign ??= _atsign;
+    if (atClientServiceMap.containsKey(atsign)) {
+      return atClientServiceMap[atsign].atClient;
+    }
+    return null;
+  }
+
+  AtClientService _getClientServiceForAtSign(String atsign) {
+    if (atClientServiceMap.containsKey(atsign)) {
+      return atClientServiceMap[atsign];
+    }
+    final service = AtClientService();
+    return service;
+  }
+
+  Future<AtClientPreference> _getAtClientPreference({String cramSecret}) async {
+    final appDocumentDirectory =
+        await path_provider.getApplicationSupportDirectory();
+    final path = appDocumentDirectory.path;
+    final _atClientPreference = AtClientPreference()
+      ..isLocalStoreRequired = true
+      ..commitLogPath = path
+      ..cramSecret = cramSecret
+      ..namespace = AtConfig.namespace
+      ..syncStrategy = SyncStrategy.IMMEDIATE
+      ..rootDomain = AtConfig.root
+      ..hiveStoragePath = path;
+    return _atClientPreference;
+  }
+
+  _checkAtSignStatus(String atsign) async {
+    final atStatusImpl = AtStatusImpl(rootUrl: AtConfig.root);
+    final status = await atStatusImpl.get(atsign);
+    return status.serverStatus;
+  }
+
+  Future<bool> onboard({String atsign}) async {
+    atClientServiceInstance = _getClientServiceForAtSign(atsign);
+    final atClientPreference = await _getAtClientPreference();
+    final result = await atClientServiceInstance.onboard(
+        atClientPreference: atClientPreference, atsign: atsign);
+    _atsign = atsign == null ? await this.getAtSign() : atsign;
+    atClientServiceMap.putIfAbsent(_atsign, () => atClientServiceInstance);
+    _sync();
+    return result;
+  }
+
+  ///Returns `false` if fails in authenticating [atsign] with [cramSecret]/[privateKey].
+  Future<bool> authenticate(
+    String atsign, {
+    String privateKey,
+    String jsonData,
+    String decryptKey,
+  }) async {
+    final atsignStatus = await _checkAtSignStatus(atsign);
+    if (atsignStatus != ServerStatus.teapot &&
+        atsignStatus != ServerStatus.activated) {
+      throw atsignStatus;
+    }
+    final atClientPreference = await _getAtClientPreference();
+    final result = await atClientServiceInstance.authenticate(
+        atsign, atClientPreference,
+        jsonData: jsonData, decryptKey: decryptKey);
+    _atsign = atsign;
+    atClientServiceMap.putIfAbsent(_atsign, () => atClientServiceInstance);
+    await _sync();
+    return result;
+  }
+
+  String encryptKeyPairs(String atsign) {
+    final encryptedPkamPublicKey = EncryptionUtil.encryptValue(
+        at_demo_data.pkamPublicKeyMap[atsign], at_demo_data.aesKeyMap[atsign]);
+    final encryptedPkamPrivateKey = EncryptionUtil.encryptValue(
+        at_demo_data.pkamPrivateKeyMap[atsign], at_demo_data.aesKeyMap[atsign]);
+    final aesencryptedPkamPublicKey = EncryptionUtil.encryptValue(
+        at_demo_data.encryptionPublicKeyMap[atsign],
+        at_demo_data.aesKeyMap[atsign]);
+    final aesencryptedPkamPrivateKey = EncryptionUtil.encryptValue(
+        at_demo_data.encryptionPrivateKeyMap[atsign],
+        at_demo_data.aesKeyMap[atsign]);
+    var aesEncryptedKeys = {};
+    aesEncryptedKeys[BackupKeyConstants.AES_PKAM_PUBLIC_KEY] =
+        encryptedPkamPublicKey;
+
+    aesEncryptedKeys[BackupKeyConstants.AES_PKAM_PRIVATE_KEY] =
+        encryptedPkamPrivateKey;
+
+    aesEncryptedKeys[BackupKeyConstants.AES_ENCRYPTION_PUBLIC_KEY] =
+        aesencryptedPkamPublicKey;
+
+    aesEncryptedKeys[BackupKeyConstants.AES_ENCRYPTION_PRIVATE_KEY] =
+        aesencryptedPkamPrivateKey;
+
+    final keyString = jsonEncode(Map<String, String>.from(aesEncryptedKeys));
+    return keyString;
+  }
+
+  Future<String> get(AtKey atKey) async {
+    final result = await _getAtClientForAtsign().get(atKey);
+    return result.value;
+  }
+
+  Future<bool> put(AtKey atKey, String value) async =>
+      await _getAtClientForAtsign().put(atKey, value);
+
+  Future<bool> delete(AtKey atKey) async =>
+      await _getAtClientForAtsign().delete(atKey);
+
+  Future<List<AtKey>> getAtKeys({String sharedBy}) async =>
+      await _getAtClientForAtsign().getAtKeys(
+        regex: AtConfig.namespace,
+        sharedBy: sharedBy,
+      );
+
+  Future<String> getAtSign() async => _atsign;
+}
+
+class BackupKeyConstants {
+  static const String AES_PKAM_PUBLIC_KEY = 'aesPkamPublicKey';
+  static const String AES_PKAM_PRIVATE_KEY = 'aesPkamPrivateKey';
+  static const String AES_ENCRYPTION_PUBLIC_KEY = 'aesEncryptPublicKey';
+  static const String AES_ENCRYPTION_PRIVATE_KEY = 'aesEncryptPrivateKey';
+}
 
 void main() {
-  print('hello from flutter');
-}
-// void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  MethodChannel _methodChannel = MethodChannel('atbridge');
+  var backend = AtService.getInstance();
+  _methodChannel?.setMethodCallHandler((call) {
+    if (call.method == 'getCurrentUser') {
+      return backend.get(call.arguments);
+    } else if (call.method == 'login') {
+      return backend.authenticate(call.arguments);
+    } else if (call.method == 'registerRider') {
+      return backend.put(call.arguments[0], call.arguments[1]);
+    } else {
+      return null;
+    }
+  });
 }
